7353f0258f235a604bf66e631803ba3cf428cd20
0x0Louis <louis@joepegs.com>
2022-10-18 13:47:08 +0200

    add basic tests

---
+++ test/TestHelper.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "forge-std/Test.sol";
+import "joe-v2/interfaces/ILBRouter.sol";
+import "joe-v2/interfaces/IJoeRouter02.sol";
+import "openzeppelin/token/ERC20/IERC20.sol";
+
+import "../src/JoeDexLens.sol";
+import "./mocks/ERC20MockDecimals.sol";
+
+abstract contract TestHelper is Test {
+    using Math512Bits for uint256;
+    address payable internal constant DEV = payable(0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84);
+    //address public TokenOwner = 0x4f029B3faA0fE6405Ae6eBA5795293688cf69c2e; // before transfer to bad multisig
+    address public TokenOwner = 0xFFC08538077a0455E0F4077823b1A0E3e18Faf0b;
+    address public LBFactory = 0x2950b9bd19152C91d69227364747b3e6EFC8Ab7F;
+    address public LBRouter = 0x0C344c52841d3F8d488E1CcDBafB42CE2C7fdFA9;
+    address public LBQuoter = 0x0C926BF1E71725eD68AE3041775e9Ba29142dca9;
+    address public FactoryV1 = 0xF5c7d9733e5f53abCC1695820c4818C59B457C2C;
+    address public RouterV1 = 0xd7f655E3376cE2D7A2b08fF01Eb3B1023191A901;
+
+    address public USDT = 0xAb231A5744C8E6c45481754928cCfFFFD4aa0732;
+    address public USDC = 0xB6076C93701D6a07266c31066B298AeC6dd65c2d;
+    address public WAVAX = 0xd00ae08403B9bbb9124bB305C09058E32C39A48c;
+
+    address public USDCUSDTv1 = 0x8625feb95141008FE48ea5cf8A7dd84A83a72d9E;
+    address public AVAXUSDCv1 = 0x9371619C8E2A487D57FB9F8E36Bcb0317Bff0529;
+
+    address public USDCUSDT1bps = 0x0716FBE78331932d0Fd9A284b22F0342a6FD8ee8;
+    address public AVAXUSDC10bps = 0x1579647e8cc2338111e131A01AF62d85870A659b;
+    address public AVAXUSDC20bps = 0xc8aa3bF8623C35EAc518Ea82B55C2aa46D5A02f6;
+
+    JoeDexLens public lens;
+}
---
+++ test/testJoeDexLens.t.sol
@@ -0,0 +1,105 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "joe-v2/interfaces/ILBRouter.sol";
+
+import "../src/JoeDexLens.sol";
+import "./TestHelper.sol";
+
+contract TestJoeDexLens2 is TestHelper {
+    function setUp() public {
+        //vm.createSelectFork(vm.rpcUrl("fuji"), 14_541_000);
+        vm.createSelectFork(vm.rpcUrl("fuji"), 14_625_546);
+        lens = new JoeDexLens(LBRouter, FactoryV1, WAVAX, USDC);
+        JoeDexLens.Market[] memory markets = new JoeDexLens.Market[](2);
+        markets[0] = JoeDexLens.Market(USDCUSDT1bps, JoeDexLens.MarketType.V2);
+        markets[1] = JoeDexLens.Market(USDCUSDTv1, JoeDexLens.MarketType.V1);
+        lens.addUSDMarkets(markets);
+
+        JoeDexLens.Market[] memory USDMarkets = lens.getUSDMarkets(USDT);
+
+        JoeDexLens.Market[] memory markets2 = new JoeDexLens.Market[](2);
+        markets2[0] = JoeDexLens.Market(AVAXUSDCv1, JoeDexLens.MarketType.V1);
+        markets2[1] = JoeDexLens.Market(AVAXUSDC10bps, JoeDexLens.MarketType.V2);
+
+        lens.addAVAXMarkets(markets2);
+        JoeDexLens.Market[] memory AVAXMarkets = lens.getAVAXMarkets(USDC);
+    }
+
+    function testUSDPrice() public {
+        // Price of USDT in USDC will increase, when selling USDC
+        uint256 tokenAmount = 50_000e6;
+        uint256 USDTPrice1 = lens.getTokenPriceUSD(USDT);
+        console2.log(USDTPrice1);
+        ERC20MockDecimals tokenUSDC = ERC20MockDecimals(USDC);
+
+        vm.prank(TokenOwner);
+        tokenUSDC.mint(DEV, tokenAmount);
+        tokenUSDC.transfer(USDCUSDT1bps, tokenAmount);
+
+        vm.prank(TokenOwner);
+        tokenUSDC.mint(DEV, tokenAmount);
+        ILBPair(USDCUSDT1bps).swap(true, DEV);
+
+        uint256 USDTPrice2 = lens.getTokenPriceUSD(USDT);
+        console2.log(USDTPrice2);
+        assertGt(USDTPrice2, USDTPrice1);
+        address[] memory path = new address[](2);
+        path[0] = USDC;
+        path[1] = USDT;
+        tokenUSDC.approve(RouterV1, tokenAmount);
+        IJoeRouter01(RouterV1).swapExactTokensForTokens(tokenAmount, 0, path, DEV, block.timestamp);
+        uint256 USDTPrice3 = lens.getTokenPriceUSD(USDT);
+        console2.log(USDTPrice3);
+        assertGt(USDTPrice3, USDTPrice2);
+    }
+
+    function testAVAXPrice() public {
+        // Price of USDC in AVAX will decrease, when selling USDC
+        uint256 tokenAmount = 43_000e6;
+        uint256 USDCPrice1 = lens.getTokenPriceAVAX(USDC);
+        console2.log("USDCPrice1", USDCPrice1);
+        ERC20MockDecimals tokenUSDC = ERC20MockDecimals(USDC);
+
+        vm.prank(TokenOwner);
+        tokenUSDC.mint(DEV, tokenAmount);
+        tokenUSDC.transfer(AVAXUSDC10bps, tokenAmount);
+        ILBPair(AVAXUSDC10bps).swap(false, DEV);
+
+        uint256 USDCPrice2 = lens.getTokenPriceAVAX(USDC);
+        console2.log("USDCPrice2", USDCPrice2);
+        assertLt(USDCPrice2, USDCPrice1);
+        address[] memory path = new address[](2);
+        path[0] = USDC;
+        path[1] = WAVAX;
+        console2.log("getPriceFromV1", getPriceFromV1(AVAXUSDCv1, USDC));
+        vm.prank(TokenOwner);
+        tokenAmount = 4e18;
+        tokenUSDC.mint(DEV, tokenAmount);
+        tokenUSDC.approve(RouterV1, tokenAmount);
+        IJoeRouter01(RouterV1).swapExactTokensForTokens(tokenAmount, 0, path, DEV, block.timestamp);
+        //IJoeRouter01(RouterV1).swapExactAVAXForTokens{value: tokenAmount}(0, path, DEV, block.timestamp);
+        uint256 USDCPrice3 = lens.getTokenPriceAVAX(USDC);
+        console2.log("USDCPrice3", USDCPrice3);
+        console2.log("getPriceFromV1", getPriceFromV1(AVAXUSDCv1, USDC));
+        // fails - no idea why. getPriceFromV1 decreases while getTokenPriceAVAX increases?
+        assertLt(USDCPrice3, USDCPrice2);
+    }
+
+    function getPriceFromV1(address pairAddress, address token) internal view returns (uint256) {
+        uint256 PRECISION = 1e18;
+        IJoePair pair = IJoePair(pairAddress);
+        address token0 = pair.token0();
+        address token1 = pair.token1();
+        uint256 decimals0 = ERC20(token0).decimals();
+        uint256 decimals1 = ERC20(token1).decimals();
+
+        (uint256 reserve0, uint256 reserve1, ) = pair.getReserves();
+        if (token == token0) {
+            return (PRECISION * reserve1 * 10**decimals0) / (reserve0 * 10**decimals1);
+        } else if (token == token1) {
+            return (PRECISION * reserve0 * 10**decimals1) / (reserve1 * 10**decimals0);
+        }
+    }
+}
