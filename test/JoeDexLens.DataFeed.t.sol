edf8f1a70ca284423694e288891df9657189447e
Louis <107303182+0x0Louis@users.noreply.github.com>
2024-06-17 15:32:03 +0200

    Revert "Update tests"
    This reverts commit 613ab467310dcc1ce39dd97e08b366352f4c68e0.

--- test/JoeDexLens.DataFeed.t.sol
+++ test/JoeDexLens.DataFeed.t.sol
@@ -19,9 +19,12 @@ contract TestJoeDexLens is TestHelper {
 
         joeDexLens = JoeDexLens(address(new TransparentUpgradeableProxy(address(imp), address(1), "")));
 
-        IJoeDexLens.DataFeed[] memory dataFeeds = new IJoeDexLens.DataFeed[](2);
+        IJoeDexLens.DataFeed[] memory dataFeeds = new IJoeDexLens.DataFeed[](1);
         dataFeeds[0] = IJoeDexLens.DataFeed(wNative, AVAX_USDC_20BP, 1000, IJoeDexLens.DataFeedType.V2_1);
-        dataFeeds[1] = IJoeDexLens.DataFeed(wNative, AVAX_USDT_20BP, 100, IJoeDexLens.DataFeedType.V2_1);
+
+        // IJoeDexLens.DataFeed[] memory dataFeeds = new IJoeDexLens.DataFeed[](2);
+        // dataFeeds[0] = IJoeDexLens.DataFeed(wNative, AVAX_USDC_20BP, 1000, IJoeDexLens.DataFeedType.V2_1);
+        // dataFeeds[1] = IJoeDexLens.DataFeed(wNative, AVAX_USDT_20BP, 100, IJoeDexLens.DataFeedType.V2_1);
 
         joeDexLens.initialize(dataFeeds);
     }
